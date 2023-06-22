3ff38e085261a5a9903a30bcf058118ed4838530
KenilViradiya <129634846+KenilViradiya@users.noreply.github.com>
2023-06-22 10:42:55 +0530

    Quiz App

---
+++ app/src/main/java/com/example/quizappobi/Qns.java
@@ -0,0 +1,26 @@
+package com.example.quizappobi;
+
+public class Qns {
+    public static String que[]= {
+            "Which river is considered the holiest river in India?",
+            "Which Indian state is known as the 'Land of the Gods'?",
+            "What is the currency of India?",
+            "Which festival is known as the Festival of Lights in India?",
+            "Which sport is the national game of India?",
+    };
+    public static  String choice[][]= {
+            {"Gangas", "Yamuna", "Brahmaputra"},
+            {"Uttarakhand", "Himachal Pradesh", "Jammu and Kashmir"},
+            {"Indian Rupee", "Dollar", "Euro"},
+            {"Diwali", "Holi", "Eid"},
+            {"Hockey", "Cricket", "Football"},
+
+    };
+    public static  String correctans[]={
+            "Gangas",
+            "Uttarakhand",
+            "Indian Rupee",
+            "Diwali",
+            "Hockey"
+    };
+}
